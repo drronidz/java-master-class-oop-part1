094de55101a9773435026dad80224f9a91274483
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-04-09 13:41:48 +0100

    issue done. closes #2.

---
+++ Classes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.model = "Carrera";
+
+    }
+}
